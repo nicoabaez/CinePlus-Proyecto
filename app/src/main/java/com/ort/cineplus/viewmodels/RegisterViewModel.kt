14330eb3ec1ec069137676c679da133d3057d895
jmgiannotti <juanmartingiannotti@gmail.com>
2023-06-15 18:19:32 -0300

    Revert "refactor del registro"
    This reverts commit 9e7efdd765b1c2e10e554f98ca68c08048d4d4ba.

--- app/src/main/java/com/ort/cineplus/viewmodels/RegisterViewModel.kt
+++ app/src/main/java/com/ort/cineplus/viewmodels/RegisterViewModel.kt
@@ -2,111 +2,66 @@ package com.ort.cineplus.viewmodels
 
 import android.content.ContentValues.TAG
 import android.util.Log
-import android.widget.EditText
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import com.ort.cineplus.entities.User
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
 
 class RegisterViewModel : ViewModel() {
     var database = Firebase.firestore
     private val auth: FirebaseAuth = FirebaseAuth.getInstance();
+    private var users = mutableListOf<User>()
 
-
-    fun authRegister(user: User){
+    fun authRegister(user: User): Boolean{
+        var result = false;
+        if(!checkEmail(user.email)){
             auth.createUserWithEmailAndPassword(user.email, user.pass)
                 .addOnCompleteListener() { task ->
                     if (task.isSuccessful) {
-                        this.addUser(user)
-
+                        database.collection("Users").document(auth.uid.toString())
+                            .set(user)
+                            .addOnSuccessListener { documentReference ->
+                                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference}")
+                            }
+                            .addOnFailureListener { e ->
+                                Log.w(TAG, "Error adding document", e)
+                            }
                     } else {
                         Log.w(TAG, "createUserWithEmail:failure", task.exception)
                     }
                 }
+            result = true
+        }
+        return result;
     }
 
-     fun addUser(user: User){
-        database.collection("Users").document(auth.uid.toString())
-            .set(user)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot added with ID: $documentReference")
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error adding document", e)
-            }
-
-
-    }
-
-
-
-    private suspend fun emailAlreadyRegistered(email: String): Boolean {
-        return withContext(Dispatchers.IO) {
-            val auth = FirebaseAuth.getInstance()
-            try {
-                val result = auth.fetchSignInMethodsForEmail(email).await()
-                val signInMethods = result.signInMethods
-                signInMethods != null && signInMethods.isNotEmpty()
-            } catch (e: Exception) {
-
-                false
+    fun checkPass(pass: String, confirmPass: String): Boolean {
+        var result = false;
+        if(pass == confirmPass){
+            if(pass.length >= 6){
+                result = true;
             }
         }
+        return result;
     }
 
-    suspend fun emailIsNotUsed(email: EditText): Boolean {
-        var boolean = false
-        val emailRegistered = emailAlreadyRegistered(email.text.toString())
-        if (isNotEmpty(email)){
-            if (emailRegistered) {
-                showError(email, "Email ya registrado")
-                clearInput(email)
-
-            } else {
-                boolean = true
-            }
-        }
-        return boolean
-    }
-
-    fun checkPass(confirmPass: EditText, pass: EditText): Boolean {
-        var boolean = false
-        if (isNotEmpty(pass) && isNotEmpty(confirmPass)) {
-            if (pass.text.length >= 6) {
-                if (confirmPass.text.toString().equals(pass.text.toString())) {
-                    boolean = true
-                } else {
-                    showError(confirmPass, "Las contraseñas no coinciden")
-
+    private fun checkEmail(email: String) : Boolean {
+        var result = false;
+        database.collection("Users")
+            .get().addOnSuccessListener { documents ->
+                for(document in documents){
+                    users.add(document.toObject())
                 }
-
-            } else {
-                showError(confirmPass, "La contraseña debe tener 6 o mas caracteres")
             }
+
+        val userSearched = users.firstOrNull { it.email == email }
+        Log.d("usuario:", "usuario encontrado: $userSearched")
+        if(userSearched != null){
+            result = true ;
         }
-
-        return boolean
-    }
-    private fun clearInput(editText: EditText) {
-        editText.setText("")
-    }
-
-    private fun showError(editText: EditText, s: String) {
-        editText.error = s
-        editText.clearFocus()
-    }
-
-    private fun isNotEmpty(editText: EditText): Boolean {
-        var valid = true
-        if(editText.text.isEmpty()){
-            showError(editText,"Campo requerido" )
-            valid = false
-        }
-        return valid
+        return result;
     }
 
 }
